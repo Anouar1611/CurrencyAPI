190c5593ed2b29727ecd5ec1184204c3f1d4610e
Anouar1611 <you@example.com>
2024-05-03 14:56:43 +0200

    Added OpenApi docs to the API.

--- src/main/java/com/currencyapi/example/web/controller/CurrencyConverterController.java
+++ src/main/java/com/currencyapi/example/web/controller/CurrencyConverterController.java
@@ -1,33 +1,40 @@
 package com.currencyapi.example.web.controller;
 
+import com.currencyapi.example.request.ConvertRequest;
 import com.currencyapi.example.service.CurrencyConverter;
 import com.currencyapi.example.service.CurrencyServiceImpl;
-import com.currencyapi.example.request.ConvertRequest;
-import org.springframework.cache.annotation.Cacheable;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
 import java.util.Map;
 
 @RestController
 public class CurrencyConverterController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyConverterController.class);
     private final CurrencyConverter currencyConverter;
-
     private final CurrencyServiceImpl currencyServiceImpl;
 
-    public CurrencyConverterController(
-            CurrencyConverter currencyConverter,
-            CurrencyServiceImpl currencyServiceImpl
-    ) {
+    public CurrencyConverterController(CurrencyConverter currencyConverter, CurrencyServiceImpl currencyServiceImpl) {
         this.currencyConverter = currencyConverter;
         this.currencyServiceImpl = currencyServiceImpl;
     }
 
     @PostMapping("/convert")
+    @Operation(summary = "Converts a given amount to the specified currency",
+            description = "Provides real-time currency conversion based on the supplied amount and target currency.")
+    @ApiResponse(responseCode = "200", description = "Successful conversion",
+            content = @Content(schema = @Schema(implementation = Double.class)))
+    @ApiResponse(responseCode = "400", description = "Invalid input provided")
     public Mono<ResponseEntity<?>> convertEurAmount(@RequestBody ConvertRequest request) {
+        LOGGER.info("Calling converting endpoint POST convert");
         try {
             double amount = Double.parseDouble(request.getAmountToConvert().toString());
             return currencyConverter.convert(amount, request.getTargetCurrency())
@@ -37,11 +44,13 @@ public class CurrencyConverterController {
         }
     }
 
-
-    @Cacheable("currenciesCache")
     @GetMapping("/currencies")
+    @Operation(summary = "Fetches all available currency names",
+            description = "Retrieves a map of currency codes to their full names.")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of currency names",
+            content = @Content(schema = @Schema(implementation = Map.class)))
     public Mono<Map<String, String>> getAllCurrenciesNames() {
+        LOGGER.info("Calling currencies endpoint GET currencies");
         return currencyServiceImpl.getFullCurrencyNames();
     }
-
 }
